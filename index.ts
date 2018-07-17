e65f5363ab7f5d89f79532d8a5597bfa3a9e3e2f
Alex Szabó <kreatemore@gmail.com>
2018-07-17 17:13:33 +0200

    📖 Fixed code tags in docs

--- index.ts
+++ index.ts
@@ -10,57 +10,51 @@ export class Optional<T> {
   }
 
   /**
-   * Returns an {@code Optional} describing the given non-{@code null}
+   * Returns an `Optional` describing the given non-`null`
    * value.
    *
-   * @param value the value to describe, which must be non-{@code null}
+   * @apiNote
+   * Though it may be tempting to do so, avoid testing if an object is empty by
+   * comparing with `==` against instances returned by `Optional.empty()`,
+   * use {@link isPresent} instead.
+   *
+   * @param value the value to describe, which must be non-`null`
    * @param <T> the type of the value
-   * @return an {@code Optional} with the value present
-   * @throws NullValueException if value is {@code null}
+   * @return an `Optional` with the value present
+   * @throws `NullValueException` if value is `null`
    */
   static of<T>(value: T): Optional<T> {
     return new Optional(value);
   }
 
   /**
-   * Returns an {@code Optional} describing the given value, if
-   * non-{@code null}, otherwise returns an empty {@code Optional}.
+   * Returns an `Optional` describing the given value, if
+   * non-`null`, otherwise returns an empty `Optional`.
    *
-   * @param value the possibly-{@code null} value to describe
+   * @param value the possibly-`null` value to describe
    * @param <T> the type of the value
-   * @return an {@code Optional} with a present value if the specified value
-   *         is non-{@code null}, otherwise an empty {@code Optional}
+   * @return an `Optional` with a present value if the specified value
+   *         is non-`null`, otherwise an empty `Optional`
    */
   static ofNullable<T>(value: T | null): Optional<T> {
     return new Optional<T>(value, true);
   }
 
   /**
-   * Returns an empty {@code Optional} instance.  No value is present for this
-   * {@code Optional}.
-   *
-   * @apiNote
-   * Though it may be tempting to do so, avoid testing if an object is empty
-   * by comparing with {@code ==} against instances returned by
-   * {@code Optional.empty()}.  There is no guarantee that it is a singleton.
-   * Instead, use {@link isPresent()}.
+   * Returns an empty `Optional` instance.  No value is present for this
+   * `Optional`.
    *
    * @param <T> The type of the non-existent value
-   * @return an empty {@code Optional}
+   * @return an empty `Optional`
    */
   static empty<T>(): Optional<T> {
     return new Optional<T>();
   }
 
   /**
-   * Returns a non-empty string representation of this {@code Optional}
+   * Returns a non-empty string representation of this `Optional`
    * suitable for debugging.  The exact presentation format is unspecified and
    * may vary between implementations and versions.
-   *
-   * @implSpec
-   * If a value is present the result must include its string representation
-   * in the result.  Empty and present {@code Optional}s must be unambiguously
-   * differentiable.
    *
    * @return the string representation of this instance
    */
@@ -75,9 +69,9 @@ export class Optional<T> {
   }
 
   /**
-   * If a value is present, returns {@code true}, otherwise {@code false}.
+   * If a value is present, returns `true`, otherwise `false`.
    *
-   * @return {@code true} if a value is present, otherwise {@code false}
+   * @return `true` if a value is present, otherwise `false`
    */
   isPresent(): boolean {
     return this.value !== null;
@@ -102,7 +96,6 @@ export class Optional<T> {
    * @param action the action to be performed, if a value is present
    * @param emptyAction the empty-based action to be performed, if no value is
    *        present
-   * @since 9
    */
   ifPresentOrElse(action: (value: T) => void, emptyAction: () => any): void {
     if (this.isPresent()) {
@@ -114,11 +107,11 @@ export class Optional<T> {
 
   /**
    * If a value is present, returns the value, otherwise returns
-   * {@code other}.
+   * `other`.
    *
    * @param other the value to be returned, if no value is present.
-   *        May be {@code null}.
-   * @return the value, if present, otherwise {@code other}
+   *        May be `null`.
+   * @return the value, if present, otherwise `other`
    */
   orElse(other: T | null): T {
     return this.isPresent() ? this.value : other;
@@ -131,8 +124,6 @@ export class Optional<T> {
    * @param supplier the supplying function that produces a value to be returned
    * @return the value, if present, otherwise the result produced by the
    *         supplying function
-   * @throws NullPointerException if no value is present and the supplying
-   *         function is {@code null}
    */
   orElseGet(supplier: () => T): T {
     return this.isPresent() ? this.value : supplier();
@@ -142,8 +133,7 @@ export class Optional<T> {
    * If a value is present, returns the value, otherwise throws an exception
    *
    * @param <X> Type of the exception to be thrown
-   * @param error the supplying function that produces an
-   *        exception to be thrown
+   * @param error the exception to be thrown
    * @return the value, if present
    * @throws X if no value is present
    */
@@ -157,13 +147,13 @@ export class Optional<T> {
 
   /**
    * If a value is present, returns the value, otherwise throws
-   * {@code NullValueException}.
+   * `NullValueException`.
    *
-   * @deprecated this is counter intuitive for Optionals,
-   * use {@link isPresent()} or {@link ifPresent()}
-   * @return the non-{@code null} value described by this {@code Optional}
-   * @throws NullValueException if no value is present
-   * @see Optional#isPresent()
+   * @deprecated this is counter intuitive for `Optional`,
+   * since it throws an error if the value is `null`, recommended to
+   * use {@link isPresent} or {@link ifPresent}, etc instead
+   * @return the non-`null` value described by this `Optional`
+   * @throws `NullValueException` if no value is present
    */
   get(): T {
     if (!this.value) {
@@ -175,13 +165,13 @@ export class Optional<T> {
 
   /**
    * If a value is present, and the value matches the given predicate,
-   * returns an {@code Optional} describing the value, otherwise returns an
-   * empty {@code Optional}.
+   * returns an `Optional` describing the value, otherwise returns an
+   * empty `Optional`.
    *
    * @param predicate the predicate to apply to a value, if present
-   * @return an {@code Optional} describing the value of this
-   *         {@code Optional}, if a value is present and the value matches the
-   *         given predicate, otherwise an empty {@code Optional}
+   * @return an `Optional` describing the value of this
+   *         `Optional`, if a value is present and the value matches the
+   *         given predicate, otherwise an empty `Optional`
    */
   filter(predicate: (value?: T) => boolean): Optional<T> {
     const result = predicate(this.value);
@@ -191,18 +181,18 @@ export class Optional<T> {
 
 
   /**
-   * If a value is present, returns an {@code Optional} describing (as if by
+   * If a value is present, returns an `Optional` describing (as if by
    * {@link ofNullable}) the result of applying the given mapping function to
-   * the value, otherwise returns an empty {@code Optional}.
+   * the value, otherwise returns an empty `Optional`.
    *
-   * <p>If the mapping function returns a {@code null} result then this method
-   * returns an empty {@code Optional}.
+   * <p>If the mapping function returns a `null` result then this method
+   * returns an empty `Optional`.
    *
    * @param mapper the mapping function to apply to a value, if present
    * @param <U> The type of the value returned from the mapping function
-   * @return an {@code Optional} describing the result of applying a mapping
-   *         function to the value of this {@code Optional}, if a value is
-   *         present, otherwise an empty {@code Optional}
+   * @return an `Optional` describing the result of applying a mapping
+   *         function to the value of this `Optional`, if a value is
+   *         present, otherwise an empty `Optional`
    */
   map<U>(mapper: (value?: T) => U): Optional<U> {
     const value = mapper(this.value);
